0b262c2b91c3bc699c1c9d20397f396a2e87c274
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2023-04-17 16:23:14 -0300

    Revertendo alterações Cartão Cliente

--- backend/backendSelfChop/backendSelfChop/src/main/java/com/doug/backendSelfChop/repository/CartaoClienteRepository.java
+++ backend/backendSelfChop/backendSelfChop/src/main/java/com/doug/backendSelfChop/repository/CartaoClienteRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface CartaoClienteRepository extends JpaRepository<CartaoCliente,Long> {
 
@@ -15,6 +14,4 @@ public interface CartaoClienteRepository extends JpaRepository<CartaoCliente,Lon
     @Query("SELECT cartao FROM CartaoCliente cartao WHERE cartao.cliente.cpf = :cpf and cartao.status = TRUE")
     CartaoCliente findByCliente(@Param("cpf")String cpf);
 
-    @Query("SELECT cartao FROM CartaoCliente cartao WHERE cartao.status = TRUE")
-    List<CartaoCliente> findOpenCart();
 }
